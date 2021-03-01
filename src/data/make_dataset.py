d2ae3608bd743f7fc0c22c1c1ae91f22a8a2d241
David Antoszyk <David.Antoszyk@gmail.com>
2021-03-01 13:27:25 -0500

    removed unneeded files, edited readme

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,22 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+## Downloads the dataset from raw.githubusercontent.com/ageron/handson-ml2/master/
+## and places it in the data/raw/folder
 
+import os, tarfile
+import urllib.request as urllib
+DOWNLOAD_PATH = "https://raw.githubusercontent.com/ageron/handson-ml2/master/datasets/housing/"
+DATASET_NAME = "housing.tgz"
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+SAVE_LOCATION = "../../data/external/"
 
-
+def fetch_housing_data(DOWNLOAD_PATH, DATASET_NAME, SAVE_LOCATION):
+    download_location = DOWNLOAD_PATH + DATASET_NAME
+    saved_file = SAVE_LOCATION + DATASET_NAME
+    
+    urllib.urlretrieve(download_location, saved_file)
+    
+    housing_tgz = tarfile.open(saved_file)
+    housing_tgz.extractall(path="../../data/raw/")
+    housing_tgz.close()
+    
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    fetch_housing_data(DOWNLOAD_PATH, DATASET_NAME, SAVE_LOCATION)
